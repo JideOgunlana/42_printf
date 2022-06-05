ce31a8da7507818132dc6513b69dacfa3da879a7
Jide_O <45870109+JideOgunlana@users.noreply.github.com>
2022-06-05 21:56:38 +0200

    Update ft_print_num.c

--- ft_print_num.c
+++ ft_print_num.c
@@ -12,6 +12,23 @@
 
 #include "ft_printf.h"
 
+void	num_point_flag(t_printf *val, char *str)
+{
+	if (strchr(str, '-'))
+	{
+		write(1, "-", 1);
+		str = ft_strtrim(str, "-");
+	}
+	while (val->point > (int) ft_strlen(str))
+	{
+		val->tl += write(1, "0", 1);
+		val->point--;
+	}
+	ft_putstr_fd(str, 1);
+	free(str);
+	return ;
+}
+
 int	check_num_flags(t_printf *val, int num, char *str, int width_count)
 {
 	if (((val->spc && val->sign) || val->sign) && num >= 0)
@@ -28,16 +45,9 @@ int	check_num_flags(t_printf *val, int num, char *str, int width_count)
 		val->spc = FALSE;
 	}
 	if (val->width > (int) ft_strlen(str))
-	val->tl += width_count;
-	if (val->point > (int) ft_strlen(str) -1)
-	{
-		val->point = val->point - (ft_strlen(str) - 1);
-		while (val->point > 0)
-		{
-			write(1, "0", 1);
-			val->point--;
-		}
-	}
+		val->tl += width_count;
+	if (val->point)
+		num_point_flag(val, str);
 	return (width_count);
 }
 
@@ -68,13 +78,13 @@ void	ft_print_num(t_printf *val)
 		return ;
 	}
 	width_count = check_num_flags(val, num, str, width_count);
-	if (num < 0 && val->zero)
+	if (num < 0 && num != -2147483648 && val->zero)
 	{
 		write(1, "-", 1);
 		num *= -1;
 	}
 	width_count = ft_width_count(val, width_count);
 	val->width = width_count;
-	val->point = 0;
-	ft_putnbr_fd(num, 1);
+	if (!val->point)
+		ft_putnbr_fd(num, 1);
 }
