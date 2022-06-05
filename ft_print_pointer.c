e6e25ea5c1f0a17838069206e5c7f15a14bb0176
Jide_O <45870109+JideOgunlana@users.noreply.github.com>
2022-06-05 21:57:49 +0200

    Update ft_print_pointer.c

--- ft_print_pointer.c
+++ ft_print_pointer.c
@@ -38,12 +38,33 @@ void	ft_put_ptr(unsigned long p, int fd)
 	}
 }
 
+void	p_dash_flag(unsigned long p, int width_count, t_printf *val)
+{
+	write(1, "0x", 2);
+	ft_put_ptr(p, 1);
+	val->tl += ft_pointer_l(p) + 2;
+	width_count = ft_width_count(val, width_count);
+	val->width = 0;
+}
+
 void	ft_print_pointer(t_printf *val)
 {
 	unsigned long	p;
+	int				width_count;
 
 	p = va_arg(val->args, unsigned long);
+	width_count = val->width - ft_pointer_l(p) - 2;
+	if (width_count > 0)
+		val->tl += width_count;
+	if (val->dash)
+	{
+		p_dash_flag(p, width_count, val);
+		val->dash = FALSE;
+		return;
+	}
 	val->tl += ft_pointer_l(p) + 2;
+	width_count = ft_width_count(val, width_count);
+	val->width = width_count;
 	write(1, "0x", 2);
 	ft_put_ptr(p, 1);
 }
